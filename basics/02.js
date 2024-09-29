9cc20d2d5e7cf871c035620e8ed0574e63115b2a
Prachis1309 <124146626+Prachis1309@users.noreply.github.com>
2024-09-29 19:02:11 +0000

    code

---
+++ basics/02.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+//alert(3+3)//we are using node js ,not browser
+console.log(3+3)//maintain readability
+console.log("Prachi")
+
+//datatypes
+let name="Prachi"
+let age=21
+let bool=true
+let state;
+let city=null
+
+/*
+number => 2 to power 53
+bigint for big websites like facebook
+string =>""
+boolean => true/false
+null=>standalone value, 0 nhi h because zero is also a value
+undefined => declare krdiya h but value assign nhi kri h
+symbol=> unique
+
+object
+*/
+console.log(typeof name);
+console.log(typeof city);//object
+
