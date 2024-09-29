9d9aab19fd9bbd236597dc3f98f3bcdaf0d5e211
Prachis1309 <124146626+Prachis1309@users.noreply.github.com>
2024-09-29 18:32:15 +0000

    change

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,19 @@
-console.log ("Prachi singh")
+const accountId=1234
+let accEmail="prachi.com"
+let accState;
+var accPassword = "1234"
+accCity = "Chandausi"
+// accountId =2 not allowed
+// accEmail="yahoo"
+// accPassword="0989"
+// accCity="Delhi"
+
+// console.log(accountId);
+console.table([accountId,accEmail,accPassword,accCity,accState])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+let change accoring to scope 
+scope means={}
+but var change its value everywhere
+*/
